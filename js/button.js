e253544dd949a4685f3abb15869dad10958a559e
Mahdi <mohammadmahdi7862728@gmail.com>
2025-03-02 11:06:35 +0600

    change style

--- js/button.js
+++ js/button.js
@@ -12,7 +12,7 @@ document.getElementById('completed-btn-1').addEventListener('click' , function()
         alert('Board Updated Sucesfully');
         document.getElementById('completed-btn-1').disabled = true;
         activity.innerHTML = 
-        `<p class="m-5 p-5 text-sm bg-teal-100 font-bold rounded-lg text-start">'You Have Complete The Task Fix Mobile Button Issue' <span id="real-time-1"></span> </p>`
+        `<p class="m-5 p-5 text-sm bg-blue-100 font-bold rounded-lg text-start">'You Have Complete The Task Fix Mobile Button Issue' <span id="real-time-1"></span> </p>`
         document.getElementById("real-time-1").innerText = new Date().toLocaleTimeString();
         document.getElementById('activity-log-1').style.display="block";
 
@@ -33,7 +33,7 @@ document.getElementById('completed-btn-2').addEventListener('click' , function()
         alert('Board Updated Sucesfully');
         document.getElementById('completed-btn-2').disabled = true;
         activity.innerHTML = 
-        `<p class="m-5 p-5 text-sm bg-teal-100 font-bold rounded-lg text-start">'You Have Complete The Task Add Dark Mode' <span id="real-time-2"></span> </p>`
+        `<p class="m-5 p-5 text-sm bg-blue-100 font-bold rounded-lg text-start">'You Have Complete The Task Add Dark Mode' <span id="real-time-2"></span> </p>`
         document.getElementById("real-time-2").innerText = new Date().toLocaleTimeString();
         document.getElementById('activity-log-2').style.display="block";
     }
@@ -53,7 +53,7 @@ document.getElementById('completed-btn-3').addEventListener('click' , function()
         alert('Board Updated Sucesfully');
         document.getElementById('completed-btn-3').disabled = true;
         activity.innerHTML = 
-        `<p class="m-5 p-5 text-sm bg-teal-100 font-bold rounded-lg text-start">'You Have Complete The Task Optimize  Home page ' <span id="real-time-3"></span> </p>`
+        `<p class="m-5 p-5 text-sm bg-blue-100 font-bold rounded-lg text-start">'You Have Complete The Task Optimize  Home page ' <span id="real-time-3"></span> </p>`
         document.getElementById("real-time-3").innerText = new Date().toLocaleTimeString();
         document.getElementById('activity-log-3').style.display="block";
     }
@@ -73,7 +73,7 @@ document.getElementById('completed-btn-4').addEventListener('click' , function()
         alert('Board Updated Sucesfully');
         document.getElementById('completed-btn-4').disabled = true;    
         activity.innerHTML = 
-        `<p class="m-5 p-5 text-sm bg-teal-100 font-bold rounded-lg text-start">'You Have Complete The Task Add new emoji 🤲' <span id="real-time-4"></span> </p>`
+        `<p class="m-5 p-5 text-sm bg-blue-100 font-bold rounded-lg text-start">'You Have Complete The Task Add new emoji 🤲' <span id="real-time-4"></span> </p>`
         document.getElementById("real-time-4").innerText = new Date().toLocaleTimeString();
         document.getElementById('activity-log-4').style.display="block";
     }
@@ -93,7 +93,7 @@ document.getElementById('completed-btn-5').addEventListener('click' , function()
         alert('Board Updated Sucesfully');
         document.getElementById('completed-btn-5').disabled = true;
         activity.innerHTML = 
-        `<p class="m-5 p-5 text-sm bg-teal-100 font-bold rounded-lg text-start">'You Have Complete The Task Integrate OpenAI API e' <span id="real-time-5"></span> </p>`
+        `<p class="m-5 p-5 text-sm bg-blue-100 font-bold rounded-lg text-start">'You Have Complete The Task Integrate OpenAI API e' <span id="real-time-5"></span> </p>`
         document.getElementById("real-time-5").innerText = new Date().toLocaleTimeString();
         document.getElementById('activity-log-5').style.display="block";
     }
@@ -113,7 +113,7 @@ document.getElementById('completed-btn-6').addEventListener('click' , function()
         alert('Board Updated Sucesfully');
         document.getElementById('completed-btn-6').disabled = true;
         activity.innerHTML = 
-        `<p class="m-5 p-5 text-sm bg-teal-100 font-bold rounded-lg text-start">'You Have Complete The Task Improve Job searching ' <span id="real-time-6"></span> </p>`
+        `<p class="m-5 p-5 text-sm bg-blue-100 font-bold rounded-lg text-start">'You Have Complete The Task Improve Job searching ' <span id="real-time-6"></span> </p>`
         document.getElementById("real-time-6").textContent = new Date().toLocaleTimeString();
         document.getElementById('activity-log-6').style.display="block";
     }
